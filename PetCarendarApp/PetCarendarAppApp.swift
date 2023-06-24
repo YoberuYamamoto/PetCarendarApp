f9fc54c5d005cfc92827c72272f9d5b76f24a1a4
YoberuYamamoto <yoberu1024@gmail.com>
2023-06-24 14:48:23 +0900

    Initial Commit

---
+++ PetCarendarApp/PetCarendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetCarendarAppApp.swift
+//  PetCarendarApp
+//
+//  Created by 山本預言留 on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PetCarendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
